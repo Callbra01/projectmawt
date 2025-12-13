77034055c5492430cc7d8a45d83f61714f4e485d
Callbra01 <000954560@mohawkcollege.ca>
2025-12-13 16:06:15 -0500

    Add Cinemachine Basic Action mapping in player movement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,68 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public InputActionAsset InputActions;
+
+    private InputAction m_moveAction;
+    private InputAction m_lookAction;
+
+    private Vector2 m_moveAmt;
+    private Vector2 m_lookAmt;
+
+    private Animator m_animator;
+    private Rigidbody m_rb;
+
+    public float walkSpeed = 5;
+    public float rotateSpeed = 5;
+    //public float jumpSpeed = 5;
+
+    private void OnEnable()
     {
-        
+        // Enable action map
+        InputActions.FindActionMap("Player").Enable();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
+        // Disable action map
+        InputActions.FindActionMap("Player").Disable();
+    }
+
+    private void Awake()
+    {
+        m_moveAction = InputSystem.actions.FindAction("Move");
+        m_lookAction = InputSystem.actions.FindAction("Look");
+
+        m_animator = GetComponent<Animator>();
+        m_rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        m_moveAmt = m_moveAction.ReadValue<Vector2>();
+        m_lookAmt = m_lookAction.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Walking();
+        Rotating();
+    }
+
+    private void Walking()
+    {
+        m_animator.SetFloat("ForwardSpeed", m_moveAmt.y);
+        m_rb.MovePosition(m_rb.position + transform.forward * m_moveAmt.y * walkSpeed * Time.deltaTime);
+    }
+
+    private void Rotating()
+    {
+        float rotationAmt = m_lookAmt.x * rotateSpeed * Time.deltaTime;
+        Quaternion deltaRot = Quaternion.Euler(0, rotationAmt, 0);
+        m_rb.MoveRotation(m_rb.rotation * deltaRot);
         
     }
 }
